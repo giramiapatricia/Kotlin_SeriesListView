5b1afc180ddd6092bd0850681eb65c66478eec7b
Maliabu <giramiapatricia61@gmail.com>
2023-06-15 14:53:54 +0300

    "my commit message"

--- app/src/main/java/com/example/kotlin_series/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_series/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-
+// let me make a change to commit
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
